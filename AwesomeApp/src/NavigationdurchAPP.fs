1ac0de6d4b0095c4e573f8c24b768ccd97cf56af
paulineHans <mrspaulinehans@gmail.com>
2024-02-27 15:20:03 +0100

    Icon austausch

--- AwesomeApp/src/NavigationdurchAPP.fs
+++ AwesomeApp/src/NavigationdurchAPP.fs
@@ -21,16 +21,17 @@ type Navigationsleiste =
             prop.children [
         Bulma.navbarBrand.div [
             Bulma.navbarItem.a [
-                Html.img [ prop.src "https://bulma.io/images/bulma-logo-white.png"; prop.height 28; prop.width 112; ]
+                Html.img [ prop.src "AwesomeApp/src/DD.jpg"; prop.height 28; prop.width 112; ]
             ]
         ]
         Bulma.navbarMenu [
             Bulma.navbarStart.div [
-                Bulma.navbarItem.a [ prop.text "Home" ]
-                Bulma.navbarItem.a [ prop.text "Documentation" ]
-                Bulma.navbarItem.a [ prop.text "Jobs" ]
-                Bulma.navbarItem.a [ prop.text "Contact" ]
-                Bulma.navbarItem.a [ prop.text "About" ]
+                Bulma.navbarItem.a [
+                     prop.text "To-Do-Liste" 
+                     ]
+                Bulma.navbarItem.a [ 
+                    prop.text "Counter" 
+                    ]
                 ]
          ]
         ]
