f2899ab0a91e016f4760d65ebda4760b41217bf0
paulineHans <mrspaulinehans@gmail.com>
2024-02-27 15:22:32 +0100

    Landing Page aus NavBar entfernt

--- AwesomeApp/src/NavigationdurchAPP.fs
+++ AwesomeApp/src/NavigationdurchAPP.fs
@@ -5,12 +5,10 @@ open Feliz.Bulma
 module Types = 
     [<RequireQualifiedAccess>]
     type Page =
-        |LandingPage
         |Counter
         |Todo
         member this.toStringReadable () =
             match this with
-            |LandingPage -> "Start"
             |Counter -> "Counter" 
             |Todo -> "Todo-Liste" //zu einem lesbaren string umwandeln (wird angezeigt auf der website) 
 
@@ -43,7 +41,4 @@ type Navigationsleiste =
      
     static member Todo(setPage) =   
         Navigationsleiste.Subpagelink(Types.Page.Todo, setPage)  //verlinkt den richten suppage link mit dem typen
-
-    static member LandingPage (setPage ) = 
-        Navigationsleiste.Subpagelink(Types.Page.LandingPage, setPage)
     
