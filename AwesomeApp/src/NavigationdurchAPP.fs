56706272cc6c177d6bc4f1817a624ef4e5d66e67
paulineHans <mrspaulinehans@gmail.com>
2024-02-28 14:31:15 +0100

    Einfügen von Bluma NavBar

--- AwesomeApp/src/NavigationdurchAPP.fs
+++ AwesomeApp/src/NavigationdurchAPP.fs
@@ -16,3 +16,27 @@ module Types =
             |Counter -> "Counter" 
             |Todo -> "Todo-Liste" 
 
+//Code für das Aussehen der Navigationsleiste 
+    type NavBar  =
+        static member private Subpagelink (setPage: Page -> unit, statepage: Page) = 
+            Bulma.navbar [
+                Bulma.color.isWhite
+                prop.children [
+                    Bulma.navbarBrand.div [
+                        Bulma.navbarItem.a [
+                            Html.img [ prop.src "https://bulma.io/images/bulma-logo-white.png"; prop.height 28; prop.width 112; ]
+                        ]
+                    ]
+                    Bulma.navbarMenu [
+                        Bulma.navbarStart.div [
+                            Bulma.navbarItem.a [ prop.text "Home" ]
+                            Bulma.navbarItem.a [ prop.text "Documentation" ]
+                            Bulma.navbarItem.a [ prop.text "Jobs" ]
+                            Bulma.navbarItem.a [ prop.text "Contact" ]
+                            Bulma.navbarItem.a [ prop.text "About" ]
+                        ]
+                    ]
+                ]
+            ]
+    
+
