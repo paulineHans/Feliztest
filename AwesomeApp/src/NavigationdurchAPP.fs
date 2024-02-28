dba3cf6303d12b6361c52dcfde26d5a9695a5fb9
paulineHans <mrspaulinehans@gmail.com>
2024-02-28 14:16:08 +0100

    Überarbeitung

--- AwesomeApp/src/NavigationdurchAPP.fs
+++ AwesomeApp/src/NavigationdurchAPP.fs
@@ -4,45 +4,15 @@ open Feliz.Bulma
 
 module Types = 
     [<RequireQualifiedAccess>]
-    type Page =
+    type Page =     //type Page repräsentiert verschiedene Fallalternativen 
         |LandingPage
         |Counter
         |Todo
-        member this.toStringReadable () =
+    
+    // member this.toStrinReadable wandelt alles in einen String um, sodass es Lesbar wird auf Website 
+        member this.toStringReadable () = 
             match this with
             |LandingPage -> "Start"
             |Counter -> "Counter" 
-            |Todo -> "Todo-Liste" //zu einem lesbaren string umwandeln (wird angezeigt auf der website) 
+            |Todo -> "Todo-Liste" 
 
-type Navigationsleiste =
-    static member private Subpagelink (targetpage: Types.Page, setPage: Types.Page -> unit) =
-        Bulma.navbar [
-            Bulma.color.isPrimary
-            prop.children [
-                Bulma.navbarBrand.div [
-                    Bulma.navbarItem.a [  
-                    ]
-                ]
-                Bulma.navbarMenu [
-                    Bulma.navbarStart.div [
-                        Bulma.navbarItem.a [
-                            prop.text "To-Do-Liste" 
-                            ]
-                        Bulma.navbarItem.a [ 
-                            prop.text "Counter" 
-                            ]
-                        ]
-                ]
-                ]
-            ]
-           
-    static member Counter(setPage) = 
-           Navigationsleiste.Subpagelink(Types.Page.Counter, setPage)
-              //verlinkt den richten suppage link mit dem typen
-     
-    //static member Todo(setPage) =   
-       // Navigationsleiste.Subpagelink(Types.Page.Todo, setPage)  //verlinkt den richten suppage link mit dem typen
-
-    //static member LandingPage (setPage ) = 
-       // Navigationsleiste.Subpagelink(Types.Page.LandingPage, setPage)
-    
