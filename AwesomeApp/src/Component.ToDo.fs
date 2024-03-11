91578cf26c981bfc2da1d205d962ac2366ad6662
paulineHans <mrspaulinehans@gmail.com>
2024-03-11 13:00:12 +0100

    clean up

--- AwesomeApp/src/Component.ToDo.fs
+++ AwesomeApp/src/Component.ToDo.fs
@@ -3,23 +3,18 @@ open Feliz
 open Feliz.Bulma
 open Fable.SimpleJson
 
-
 module Bestandteile = //Recordtype mit zwei Feldern
-
     type Komponenten = {
         Aufgaben: string
         Checkbox: bool   
     }
+
     let localStoragekey = "TODO"
     
-    //Die Variable input enthält den aktuellen Wert dieses Zustands, 
-    //während setinput eine Funktion ist, die verwendet wird, um diesen Wert zu aktualisieren 
-
     let setToDoLocalStorange (data: Komponenten list) = 
         let JSONString = Json.stringify data
         Browser.WebStorage.localStorage.setItem (localStoragekey, JSONString)
 
-     /// backfromString dient dazu JSON.string Daten aus dem localstroage zu lesen und in der Console auszugeben und anschließend zu deserialisieren  
     let getToDoLocalStorage () = 
         let JSONString = Browser.WebStorage.localStorage.getItem (localStoragekey)
         Browser.Dom.console.log (isNull JSONString)
@@ -32,18 +27,13 @@ module Bestandteile = //Recordtype mit zwei Feldern
                 Browser.Dom.console.log JSONString
                 match Json.tryParseAs<Komponenten list> ( JSONString) with 
                 | Ok (parsedData) -> 
-                    //printfn "Parseddata: %A" parsedData
                     parsedData
                 | Error _ -> 
                     printfn "couldnt parse"
                     [
                         {Aufgaben = ""; Checkbox = true}
                     ]
-        //sprintfn "result %A" result
-        result
-            
-                
-    
+        result 
     let updateListElement (table: Komponenten list) (i : int) (x : string) : Komponenten list = 
         table 
         |> List.mapi (fun (idx: int) (item: Komponenten) -> 
@@ -54,22 +44,15 @@ module Bestandteile = //Recordtype mit zwei Feldern
             else 
                 item
         )
-   
 
 open Bestandteile
 
 type ToDo = 
     [<ReactComponent>]
-    static member ToDoListe () = 
-
-//Die Funktion backfromString lädt JSON-Daten aus dem Local Storage,
-//loggt sie in der Browser-Konsole und deserialisiert sie in eine Liste von Records, 
-//wobei das Ergebnis der Deserialisierung in der Variable Recordtypeliste gespeichert wird.    
+    static member ToDoListe () =     
         let Recordtypeliste =  
-            //[{Aufgaben =  "Welche Aufgabe musst du heute erledigen?"; Erledigt = "Yay! du hast diese Aufgabe erledigt"} ]
             getToDoLocalStorage () 
         
-
         let (input, setinput) = React.useState("")
 
 
@@ -141,20 +124,8 @@ type ToDo =
             Bulma.button.button [
                 prop.text "Hinzufügen"   
                 prop.onClick (fun _ -> (
-                    {Aufgaben = input; Checkbox = false} :: table |>settable;                       
+                    {Aufgaben = input; Checkbox = false} :: table |>settable                       
                 ))
-            ]
-            Html.button [
-                prop.text "Speichern"
-                prop.onClick (fun _ ->
-                setToDoLocalStorange table)
-            ]
-            Html.button [
-                prop.text "Speicher Abrufen"
-                color.isDanger
-                prop.onClick (fun _ -> (
-                    getToDoLocalStorage () |> settable
-                ))    
-            ]                              
+            ]      
         ]
     ]
